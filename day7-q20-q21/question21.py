558e8c7cbe0fdcd5923d04675b6fcd8f94438f24
Sushant <sush1393@gmail.com>
2023-03-20 21:49:00 -0400

    Question 21 - calculate distance

---
+++ day7-q20-q21/question21.py
@@ -0,0 +1,43 @@
+'''
+Q21. A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps. The trace of robot movement is shown as the following:
+
+UP 5
+DOWN 3
+LEFT 3
+RIGHT 2
+The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer. Example: If the following tuples are given as input to the program:
+
+UP 5
+DOWN 3
+LEFT 3
+RIGHT 2
+Then, the output of the program should be:
+
+2
+'''                        
+
+import math
+print("Please provide directions: ")
+x, y = 0, 0
+dirs = []
+while True:
+    try:
+        user_input = input()
+        if not user_input:
+            break
+    except KeyboardInterrupt:
+        break
+    dirs.append(user_input)
+for i in dirs:
+    move, dist = i.split()
+    if move == "UP":
+        x += int(dist)
+    if move == "DOWN":
+        x -= int(dist)
+    if move == "RIGHT":
+        y += int(dist)
+    if move == "LEFT":
+        y -= int(dist)
+
+distance = round(math.sqrt(x**2 + y**2))
+print(distance)
