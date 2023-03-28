31d8cbd4a14270bf20b9bfc71291a42a803b7b10
Sushant <sush1393@gmail.com>
2023-03-28 19:38:20 -0400

    adding comments

--- day10-q31-q37/question32.py
+++ day10-q31-q37/question32.py
@@ -1,5 +1,6 @@
 # Q32. Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. The function should just print the keys only.
 
+# Method 1 - using traditional for loop
 def squaredict_1(n):
     square = dict()
     for i in range(1, n+1):
@@ -8,6 +9,7 @@ def squaredict_1(n):
 
 print(squaredict_1(20))
 
+# Method 2 - using dictionary comprehension
 def squaredict_2(n):
     square = {i:i ** 2 for i in range(0, n+1)}
     return square.keys()
--- day10-q31-q37/question33.py
+++ day10-q31-q37/question33.py
@@ -1,11 +1,13 @@
 # Q33. Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
 
+# Method 1 - using traditional for loop
 def squarelist_1(n):
     square = []
     for i in range(1, n+1):
         square.append(i ** 2)
     return square
 
+# Method 1 - using list comprehension
 print(squarelist_1(20))
 
 def squarelist_2(n):
